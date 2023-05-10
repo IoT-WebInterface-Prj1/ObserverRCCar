fdfa291fa818f753f9dc1c55be95da9a627a6c5a
kkang;-- global user.namekkang <ehgus8621@gmail.com>
2023-05-10 16:26:21 +0900

    uploadmedia_prototype

--- Android/app/src/main/java/com/example/myapplication/upload_media.kt
+++ Android/app/src/main/java/com/example/myapplication/upload_media.kt
@@ -1,91 +1,48 @@
-package com.example.myapplication
-
-import android.view.View
-import android.widget.TextView
-import android.widget.ImageView
-import android.app.DownloadManager
-import android.content.Context
-import android.media.ThumbnailUtils
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.provider.MediaStore.Images.Thumbnails
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.databinding.ActivityMainBinding
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.delay
+import com.example.myapplication.Media
+import com.example.myapplication.R
+import com.example.myapplication.media_adapter
 import retrofit2.Call
 import retrofit2.Callback
-import java.io.File
+import retrofit2.Retrofit
+import retrofit2.Response
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.POST
 
 
-data class Media(val title: String, val url: String, val upload_date: String)
+interface ApiService{
+    @POST("d")
+    fun getVideoList(): Call<List<Media>>
+}
 
+class upload_media: AppCompatActivity(){
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-class media_adapter(private val context: Context, private val mediaList: List<Media>)
-    : RecyclerView.Adapter<media_adapter.ViewHolder>(){
+    private val apiService = retrofit.create(ApiService::class.java)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
-        val view = LayoutInflater.from(context).inflate(R.layout.media_list, parent, false)
-        return ViewHolder(view)
-    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.media_list)
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int){
-        val media = mediaList[position]
-        holder.titleTextView.text = media.title
-        // 미디어 파일 다운로드 및 저장
-        downloadMediaFile(mediaList[position].url, holder)
-    }
+        val call = apiService.getVideoList()
+        call.enqueue(object : Callback<List<Media>> {
+            override fun onResponse(call: Call<List<Media>>, response: Response<List<Media>>){
+                if (response.isSuccessful){
+                    val mediaList = response.body()
+                    val adapter = media_adapter(mediaList)
+                    recyclerView.adapter = adapter
+                    else{
 
-    override fun getItemCount(): Int {
-        return mediaList.size
-    }
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
-        val mediaImageView: ImageView = itemView.findViewById(R.id.mediaImageView)
-    }
-
-    // 미디어 파일 다운로드 및 저장 메소드
-    private fun downloadMediaFile(url: String, holder: ViewHolder){
-        val request = DownloadManager.Request(Uri.parse(url))
-        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
-        request.setTitle("Downloading media file")
-        request.setDescription("Please wait")
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-        request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, "media.mp4")
-
-        val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        val downloadId = downloadManager.enqueue(request)
-
-        val query = DownloadManager.Query().apply { setFilterById(downloadId) }
-
-        GlobalScope.launch{
-            var downloading = true
-            while (downloading){
-                val cursor = downloadManager.query(query)
-                cursor.moveToFirst()
-                if(cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL){
-                    downloading = false
-                    val fileUri = Uri.parse(cursor.getString(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_LOCAL_URI)))
-                    val filePath = fileUri.path
-                    if(filePath != null){
-                        val file = File(filePath)
-                        val bitmap = ThumbnailUtils.createVideoThumbnail(filePath, MediaStore.Images.Thumbnails.MINI_KIND)
-                        GlobalScope.launch(Dispatchers.Main){
-                            holder.mediaImageView.setImageBitmap(bitmap)
-                        }
                     }
                 }
-                cursor.close()
-                delay(1000)
+                override fun onFailure(call: Call<List<Media>>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
             }
-        }
+        })
     }
 }
