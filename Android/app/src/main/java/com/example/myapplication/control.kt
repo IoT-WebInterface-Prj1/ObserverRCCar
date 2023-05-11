e10c66e114377389ad47095f79c3c47ad96fbf66
jihan <starlmylove@gmail.com>
2023-05-11 11:54:31 +0900

    control/streaming 추가

---
+++ Android/app/src/main/java/com/example/myapplication/control.kt
@@ -0,0 +1,73 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import org.eclipse.paho.client.mqttv3.*
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+
+class control : AppCompatActivity() {
+
+    private lateinit var mqttClient: MqttClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.control)
+
+        val brokerUrl = "tcp://localhost:1883"
+        val clientId = "android"
+        mqttClient = MqttClient(brokerUrl, clientId, MemoryPersistence())
+        mqttClient.connect()
+
+        val rightButton = findViewById<Button>(R.id.right_button)
+        rightButton.setOnClickListener {
+            publish("right")
+        }
+
+        val leftButton = findViewById<Button>(R.id.left_button)
+        leftButton.setOnClickListener {
+            publish("left")
+        }
+
+        val forwardButton = findViewById<Button>(R.id.forward_button)
+        forwardButton.setOnClickListener {
+            publish("forward")
+        }
+
+        val backwardButton = findViewById<Button>(R.id.backward_button)
+        backwardButton.setOnClickListener {
+            publish("backward")
+        }
+
+        mqttClient.subscribe("control")
+
+        mqttClient.setCallback(object : MqttCallback {
+            override fun connectionLost(throwable: Throwable?) {
+                if (throwable != null){
+                    throwable.printStackTrace()
+                }
+                try {
+                    mqttClient.reconnect()
+                } catch(ex: MqttException){
+                    ex.printStackTrace()
+                }
+            }
+
+            override fun messageArrived(topic: String?, mqttMessage: MqttMessage?) {
+                if (topic == "control") {
+                    val message = mqttMessage?.toString()
+
+                }
+            }
+
+            override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                println("Message delivered")
+            }
+        })
+    }
+
+    private fun publish(message: String) {
+        mqttClient.publish("control", MqttMessage(message.toByteArray()))
+    }
+}
+
