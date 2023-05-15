72451496f12c8eca5034fda902086438ddcf4f0d
seok <yae06067@gmail.com>
2023-05-15 16:48:44 +0900

    'commit'

--- iot_server/mjpeg/urls.py
+++ iot_server/mjpeg/urls.py
@@ -3,6 +3,9 @@ from mjpeg.views import *
 
 urlpatterns = [
     path('', CamView.as_view()),
-    path('snapshot/', snapshot, name='snapshot'),
     path('stream/', stream, name='stream'),
+    path('upload/', upload, name='upload'),
+    path('sec_file/', SecFileListView.as_view(), name='list'),
+    path('sec_file/<int:pk>', SecFileDetailView.as_view(), name='detail')
+
 ]
