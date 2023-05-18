38aec5b0cdca1e0aa586090fe436b9feb84de631
kkang;-- global user.namekkang <ehgus8621@gmail.com>
2023-05-18 17:34:04 +0900

    past

--- Android/app/src/main/java/com/example/myapplication/streaming.kt
+++ Android/app/src/main/java/com/example/myapplication/streaming.kt
@@ -1,7 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.net.http.SslError
+import android.webkit.SslErrorHandler
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.webkit.WebSettings
 import androidx.appcompat.app.AppCompatActivity
 
 class Streaming : AppCompatActivity() {
@@ -14,7 +18,14 @@ class Streaming : AppCompatActivity() {
 
         webView = findViewById(R.id.streaming)
         webView.settings.javaScriptEnabled = true
-        webView.loadUrl("http://www.naver.com")
+        webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+
+        webView.webViewClient = object : WebViewClient(){
+            override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError? ) {
+                handler?.proceed()
+            }
+        }
+        webView.loadUrl("http://172.30.1.29:8000/mjpeg/?mode=stream")
     }
 }
-//172.30.1.29:8000/mjpeg/?mode=stream
+/* 172.30.1.29:8000/mjpeg/?mode=stream */
