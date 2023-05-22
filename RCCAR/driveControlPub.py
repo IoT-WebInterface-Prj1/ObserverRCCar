d979f71f54dc7abd5804632d9cb275a5cde5e08a
yannJu <yann_ju@naver.com>
2023-05-22 16:08:19 +0900

    수정중 . . 2

--- RCCAR/driveControlPub.py
+++ RCCAR/driveControlPub.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 
+topic = "rccar/state/"
 def resultPub(_topic, client, res = 0, msg = ""):
     topic = _topic
     if len(msg) == 0:
@@ -8,3 +9,7 @@ def resultPub(_topic, client, res = 0, msg = ""):
     else: message = msg
     
     client.publish(topic, message)
+    
+def serverPub(_topic, msg, client):
+    topic += _topic
+    client.publish(topic, msg)
