95d597c89a2dcafe8b1c5c91759f849d6b2bd593
yannJu <yann_ju@naver.com>
2023-05-22 14:09:58 +0900

    캐시삭제.. . .

--- iot_server/config/urls.py
+++ iot_server/config/urls.py
@@ -21,6 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mjpeg/', include('mjpeg.urls')),
+    # path('mjpeg/', include('mjpeg.urls')),
     path('state/', include('state.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
